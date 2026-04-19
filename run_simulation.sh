3cc400910d6c0c05fe9865ff7bb9ad3df9d064e5
tuananhlevan <tuananhlevan181@gmail.com>
2026-04-20 04:35:39 +0700

    fix run.sh

--- run_simulation.sh
+++ run_simulation.sh
@@ -2,13 +2,13 @@ sup_prob=(0 0.2 0.4 0.6 0.8 1)
 env_pop=(150 300 450 600 750 900)
 model_type=(1 2)
 
-mk_dir -p saved_run
+mkdir -p saved_run
 
 for i in "${sup_prob[@]}"; do
     for j in "${env_pop[@]}"; do
         for k in "${model_type[@]}"; do
             python src/engine.py \
-                --num_sim 150 \
+                --num_sim 1000 \
                 --save_path saved_run \
                 --sup_prob $i \
                 --env_pop $j \
