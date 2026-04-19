c790c0af7c6b5a8a41f5b8e284a87e8ee3ba6d97
tuananhlevan <tuananhlevan181@gmail.com>
2026-04-20 04:33:27 +0700

    safety mk_dir in run_simulation.sh

--- run_simulation.sh
+++ run_simulation.sh
@@ -2,6 +2,8 @@ sup_prob=(0 0.2 0.4 0.6 0.8 1)
 env_pop=(150 300 450 600 750 900)
 model_type=(1 2)
 
+mk_dir -p saved_run
+
 for i in "${sup_prob[@]}"; do
     for j in "${env_pop[@]}"; do
         for k in "${model_type[@]}"; do
